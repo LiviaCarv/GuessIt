dac6863835266c9db57b64044d4a8c31d197d144
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-02-23 18:37:57 -0400

    created the GameViewModel, run the app and that a look at the logcat

--- app/src/main/java/com/example/guessit/game/GameFragment.kt
+++ app/src/main/java/com/example/guessit/game/GameFragment.kt
@@ -1,16 +1,20 @@
 package com.example.guessit.game
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.example.guessit.R
 import com.example.guessit.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+    private lateinit var viewModel: GameViewModel
     private var word = ""
     private var score = 0
 
@@ -23,11 +27,17 @@ class GameFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
 
+        // reference to GameViewModel
+        Log.i("GameFragment", "called ViewModelProvider")
+
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
         resetList()
         nextWord()
 
         binding.btnGotIt.setOnClickListener { onCorrect() }
         binding.btnSkip.setOnClickListener { onSkip() }
+        updateScoreText()
         updateWordText()
 
         return binding.root
@@ -51,12 +61,16 @@ class GameFragment : Fragment() {
         } else {
             word = wordList.removeAt(0)
         }
+        updateScoreText()
         updateWordText()
     }
 
-
     private fun updateWordText() {
         binding.txtWord.text = word
+    }
+
+    private fun updateScoreText() {
+        binding.txtCurrentScore.text = score.toString()
     }
 
 
