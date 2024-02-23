ebf8846045462c87eb251563f457b3c9e58ec576
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-02-22 20:33:59 -0400

    implementing GameFragment, adding SafeArgs and correcting navigation

--- app/src/main/java/com/example/guessit/game/GameFragment.kt
+++ app/src/main/java/com/example/guessit/game/GameFragment.kt
@@ -6,20 +6,74 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.guessit.R
-import com.example.guessit.databinding.FragmentTitleBinding
+import com.example.guessit.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+    private var word = ""
+    private var score = 0
 
-
+    private lateinit var wordList: MutableList<String>
+    private lateinit var binding: FragmentGameBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+
+        resetList()
+        nextWord()
+
+        binding.btnGotIt.setOnClickListener { onCorrect() }
+        binding.btnSkip.setOnClickListener { onSkip() }
+        updateWordText()
 
         return binding.root
     }
+
+    private fun resetList() {
+        wordList = mutableListOf(
+            "telephone",
+            "computer",
+            "key",
+            "piano",
+            "mirror",
+            "bicycle"
+        )
+        wordList.shuffle()
+    }
+
+    private fun nextWord() {
+        if (wordList.isEmpty()) {
+            gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+        updateWordText()
+    }
+
+
+    private fun updateWordText() {
+        binding.txtWord.text = word
+    }
+
+
+    private fun gameFinished() {
+        val action = GameFragmentDirections.actionGameFragmentToScoreFragment(score)
+        findNavController().navigate(action)
+    }
+
+    private fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    private fun onCorrect() {
+        score++
+        nextWord()
+    }
+
 
 }
--- app/src/main/java/com/example/guessit/score/ScoreFragment.kt
+++ app/src/main/java/com/example/guessit/score/ScoreFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.guessit.R
-import com.example.guessit.databinding.FragmentTitleBinding
+import com.example.guessit.databinding.FragmentScoreBinding
 
 class ScoreFragment : Fragment() {
 
@@ -16,9 +18,19 @@ class ScoreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_score, container, false)
+        val binding: FragmentScoreBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_score, container, false)
+
+        val scoreArgs = ScoreFragmentArgs.fromBundle(requireArguments())
+        binding.txtScore.text = scoreArgs.score.toString()
+
+        binding.btnPlayAgain.setOnClickListener { onPlayAgain() }
 
         return binding.root
+    }
+
+    private fun onPlayAgain() {
+        findNavController().navigate(ScoreFragmentDirections.actionScoreFragmentToGameFragment())
     }
 
 }
