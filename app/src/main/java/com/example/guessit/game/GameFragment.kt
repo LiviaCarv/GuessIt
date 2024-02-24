8681eeb2ce646b64905038f4b9075c43b7b9ecd6
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-02-24 07:47:12 -0400

    separating view model and fragment methods, also implemented LiveData

--- app/src/main/java/com/example/guessit/game/GameFragment.kt
+++ app/src/main/java/com/example/guessit/game/GameFragment.kt
@@ -7,18 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.example.guessit.R
 import com.example.guessit.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
     private lateinit var viewModel: GameViewModel
-    private var word = ""
-    private var score = 0
 
-    private lateinit var wordList: MutableList<String>
     private lateinit var binding: FragmentGameBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,61 +29,31 @@ class GameFragment : Fragment() {
 
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
-        resetList()
-        nextWord()
 
-        binding.btnGotIt.setOnClickListener { onCorrect() }
-        binding.btnSkip.setOnClickListener { onSkip() }
-        updateScoreText()
-        updateWordText()
+        binding.btnGotIt.setOnClickListener {
+            viewModel.onCorrect()
+        }
+        binding.btnSkip.setOnClickListener {
+            viewModel.onSkip()
+        }
+
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.txtCurrentScore.text = newScore.toString()
+        })
+
+       viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
+           binding.txtWord.text = newWord
+       })
+
 
         return binding.root
     }
 
-    private fun resetList() {
-        wordList = mutableListOf(
-            "telephone",
-            "computer",
-            "key",
-            "piano",
-            "mirror",
-            "bicycle"
-        )
-        wordList.shuffle()
-    }
-
-    private fun nextWord() {
-        if (wordList.isEmpty()) {
-            gameFinished()
-        } else {
-            word = wordList.removeAt(0)
-        }
-        updateScoreText()
-        updateWordText()
-    }
-
-    private fun updateWordText() {
-        binding.txtWord.text = word
-    }
-
-    private fun updateScoreText() {
-        binding.txtCurrentScore.text = score.toString()
-    }
-
 
     private fun gameFinished() {
-        val action = GameFragmentDirections.actionGameFragmentToScoreFragment(score)
+        val action = GameFragmentDirections.actionGameFragmentToScoreFragment(
+            viewModel.score.value ?: 0)
         findNavController().navigate(action)
-    }
-
-    private fun onSkip() {
-        score--
-        nextWord()
-    }
-
-    private fun onCorrect() {
-        score++
-        nextWord()
     }
 
 
--- app/src/main/java/com/example/guessit/game/GameViewModel.kt
+++ app/src/main/java/com/example/guessit/game/GameViewModel.kt
@@ -1,15 +1,56 @@
 package com.example.guessit.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    // inicializados como null
+    var word = MutableLiveData<String>()
+    var score = MutableLiveData<Int>()
+
+    private lateinit var wordList: MutableList<String>
+
     init {
-        Log.i("GameViewModel", "GameViewModel created!")
+        score.value = 0
+        word.value = ""
+        resetList()
+        nextWord()
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
+
+
+    private fun resetList() {
+        wordList = mutableListOf(
+            "telephone",
+            "computer",
+            "key",
+            "piano",
+            "mirror",
+            "bicycle"
+        )
+        wordList.shuffle()
+    }
+
+    private fun nextWord() {
+        if (wordList.isEmpty()) {
+            // gameFinished()
+        } else {
+            word.value = wordList.removeAt(0)
+        }
+    }
+
+    fun onSkip() {
+        score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score.value = (score.value)?.plus(1)
+        nextWord()
     }
 }
